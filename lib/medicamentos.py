a42782b7e7a3b179b3456104365bb685670695ca
Renato <renatogasparelli@pbh.gov.br>
2018-10-08 13:42:05 -0300

    atualizando: novas aquisições para estoque

--- lib/medicamentos.py
+++ lib/medicamentos.py
@@ -19,18 +19,18 @@ class __Medicamento(object):
 def obter_medicamento(nome_medicamento):
     return _HashMedicamentos[nome_medicamento]
 
-Utrogestan   = __Medicamento( 'Utrogestan'   , u'cápsula'   , dosagem='200 mg', quantidade=u'42 cápsulas')
-Metilfolato  = __Medicamento( '5-Metilfolato',  'comprimido', dosagem='1 mg')
-VitaminaC    = __Medicamento( 'VitaminaC'    , u'cápsula'   )
-BTrati       = __Medicamento( 'BTrati'       ,  'comprimido')
-Omega3       = __Medicamento( 'Omega3'       , u'cápsula'   , dosagem='DHA 300 mg e EPA 120 mg', quantidade=u'30 cápsulas')
-VitaminaE    = __Medicamento( 'VitaminaE'    ,  'comprimido')
-Somalgin     = __Medicamento( 'Somalgin'     ,  'comprimido')
-Smorfolipide = __Medicamento( 'Smorfolipide' ,  'frasco'    )
-Enoxoparina  = __Medicamento( 'Enoxoparina'  , u'injeção'   )
-Duphostan    = __Medicamento( 'Duphostan'    ,  'comprimido', dosagem='10 mg', quantidade=u'28 cápsulas')
-AdderaD3     = __Medicamento( 'AdderaD3'     ,  'comprimido')
-CalcioK2     = __Medicamento( 'CalcioK2'     ,  'comprimido')
+Utrogestan   = __Medicamento( 'Utrogestan 200 mg', u'cápsula', dosagem='200 mg', quantidade=u'42 cápsulas')
+Metilfolato  = __Medicamento( '5-Metilfolato 1 mg', 'comprimido', dosagem='1 mg')
+VitaminaC    = __Medicamento( 'VitaminaC - Cewin 500mg', u'cápsula', dosagem='500mg')
+BTrati       = __Medicamento( 'BeTrat',  'comprimido')
+Omega3       = __Medicamento( u'Ômega Mater', u'cápsula', dosagem='DHA 300 mg e EPA 120 mg', quantidade=u'30 cápsulas')
+VitaminaE    = __Medicamento( 'VitaminaE - Vita E 400mg', 'comprimido', dosagem='400mg')
+Somalgin     = __Medicamento( 'Somalgin Cardio 81mg',  'comprimido', dosagem='81mg')
+Smorfolipide = __Medicamento( 'Smorfolipide' ,  'frasco')
+Enoxoparina  = __Medicamento( 'Enoxoparina', u'injeção')
+Duphostan    = __Medicamento( 'Duphostan 10 mg',  'comprimido', dosagem='10 mg', quantidade=u'28 cápsulas')
+AdderaD3     = __Medicamento( 'AdderaD3 7.000 Ui',  'comprimido', dosagem='7.000 Ui')
+CalcioK2     = __Medicamento( 'CalcioK2',  'comprimido')
 
 
 
