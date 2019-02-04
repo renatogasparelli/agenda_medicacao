89157b318cb17f0ba09ca416b80a2002b6531a9a
Renato <renatogasparelli@pbh.gov.br>
2019-02-04 09:39:15 -0200

    ola

--- cpf.py
+++ cpf.py
@@ -6,16 +6,30 @@ import os
 import hashlib
 import json
 import codecs
-
+import datetime
 
 def main():
-    #home, jsonname = ( '/cygdrive/f/Musics', 'md5_music_f2.json')
-    home, jsonname = ( '/cygdrive/w/Music', 'md5_music_w2.json')
-    process( home, jsonname )
+    configs = [
+        ( u'/home/renato/Música', 'musicas_pbh_%(now)s.json'),
+        ( '/cygdrive/f/Musics', 'musicas_desktop_f_%(now)s.json'),
+        ( '/cygdrive/w/Music', 'musicas_hd-e_%(now)s.json')
+    ]
+    now = datetime.datetime.now()
+    data = {'now':now.strftime("%Y.%m.%d_%H.%M")}
+    for config in configs:
+        home = config[0]
+        if not os.path.exists( home ):
+            continue
+        size = len( os.listdir(home) )
+        query = u'$> Processar diretorio %s (%d items) [y/n] ? '%( home, size)
+        answer = raw_input( query.encode('utf-8') )
+        if ( answer.lower() == 'y' ):
+            print 'Processando %s'%home
+            process ( home, config[1]%data )
+        else:
+            print 'Ignorando destino %s'%home    
 
 def process(home, jsonname):
-    
-    
     
     data = []
     
